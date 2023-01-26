6e8b20d8d378d7bd512e704db07dc43bc6aceb63
dukanacv <dukanacvla@gmail.com>
2023-01-26 15:03:35 +0100

    v2.2

---
+++ src/main/java/com/example/joobleclone/config/CorsCustomConfig.java
@@ -0,0 +1,18 @@
+package com.example.joobleclone.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsCustomConfig implements WebMvcConfigurer{
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/**")
+                .allowedOrigins("https://dukanacv.github.io/jooble-clone/");
+    }
+}
